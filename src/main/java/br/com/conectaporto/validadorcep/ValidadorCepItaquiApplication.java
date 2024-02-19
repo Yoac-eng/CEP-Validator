4ef531815bc147e46cfd12a2ea2e23db6275f71e
Yoac <cayocutrim1991@gmail.com>
2024-02-19 13:05:15 -0300

    first

---
+++ src/main/java/br/com/conectaporto/validadorcep/ValidadorCepItaquiApplication.java
@@ -0,0 +1,13 @@
+package br.com.conectaporto.validadorcep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidadorCepItaquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidadorCepItaquiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/conectaporto/validadorcep/ValidadorCepItaquiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.conectaporto.validadorcep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidadorCepItaquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
